c6bd521a864a07e7b93612e67e792781420803da
AngryBugBeaRr <75998807+AngryBugBeaRr@users.noreply.github.com>
2022-03-15 00:25:26 +0400

    Add files via upload

--- Crypt3.2/code.js
+++ Crypt3.2/code.js
@@ -4,6 +4,8 @@ function crypt () {
     let chance = document.getElementById('chance').value;
     let stateCount = document.getElementById('stateCount').value;
     let mass = false;
+    let flag = false;
+    let sumCheck = 0;
     let add = '';
     let massChance = [];
     for (let j = 0; j < chance.length; j++) {
@@ -15,8 +17,7 @@ function crypt () {
             else if (chance[i] !== ',') {
                 add += chance[i];
                 i++
-            }
-            else {
+            } else {
                 massChance.push(add);
                 add = '';
                 i++
@@ -24,28 +25,59 @@ function crypt () {
         }
         massChance.push(add);
         for (let i = 0; i < massChance.length; i++) {
-            massChance[i] = Number(massChance[i]);
+            sumCheck += +massChance[i];
         }
-        if (massChance.length < stateCount)
-            for (let i = 0; i < (stateCount - massChance.length); i++) {
-                massChance.push(massChance[massChance.length - 1])
+        if (massChance.length !== +stateCount || sumCheck !== 1) {
+            flag = true;
+            document.getElementById('error').classList.add('active');
+        } else {
+            flag = false;
+            document.getElementById('error').classList.remove('active');
+        }
+        if (!flag) {
+            for (let i = 0; i < massChance.length; i++) {
+                massChance[i] = Number(massChance[i]);
             }
-        for (let i = 0; i < massChance.length; i++) {
-            if (massChance[i] === 1) {
-                result = 0;
-                break;
-            } else {
-                for (let i = 0; i < stateCount; i++) {
-                    result += massChance[i] * Math.log2(1 / massChance[i]);
+            if (massChance.length < stateCount)
+                for (let i = 0; i < (stateCount - massChance.length); i++) {
+                    massChance.push(massChance[massChance.length - 1])
+                }
+            for (let i = 0; i < massChance.length; i++) {
+                if (massChance[i] === 1) {
+                    result = 0;
+                    break;
                 }
             }
+            for (let i = 0; i < stateCount; i++) {
+                if (massChance[i] === 1) {
+                    result = 0;
+                    break;
+                }
+                result += massChance[i] * Math.log2(1 / massChance[i]);
+                console.log(Math.log2(1 / massChance[i]));
+                console.log('res: ', massChance[i] * Math.log2(1 / massChance[i]));
+            }
+        } else {
+            document.getElementById('1').innerHTML = 'Результат: ';
         }
     }
-    else {
-        for (let i = 0; i < stateCount; i++) {
-            result = Math.abs(Math.log2(chance));
-        }
+    else
+        {
+            if (chance * stateCount !== 1) {
+                flag = true;
+                document.getElementById('error').classList.add('active');
+            } else {
+                flag = false;
+                document.getElementById('error').classList.remove('active');
+            }
+            if (!flag) {
+                for (let i = 0; i < stateCount; i++) {
+                    result = Math.abs(Math.log2(chance));
+                }
+            } else {
+                document.getElementById('1').innerHTML = 'Результат: ';
+            }
+        document.getElementById('1').innerHTML = 'Результат: ' + result;
+        return result;
     }
-    document.getElementById('1').innerHTML = 'Результат: ' + result;
-    return result;
 }
